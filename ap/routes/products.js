fe1c0641efc2027ae434b20cd302abab0bd6e77a
emanuel <emanuel123.calvovilla@gmail.com>
2024-12-23 22:38:08 -0500

    Put method added and some validations

--- ap/routes/products.js
+++ ap/routes/products.js
@@ -24,7 +24,7 @@ router.post('/', (req, res) => {
     }
 
     if (!descr || typeof descr !== 'string' || descr.trim().length === 0) {
-        return res.status(400).json({ error: 'Debes ingresar una descripcion.' });
+        return res.status(400).json({ error: 'Debes ingresar una descripción.' });
     }
 
     if (!price || isNaN(price) || price <= 0) {
@@ -40,6 +40,47 @@ router.post('/', (req, res) => {
     };
     products.push(newProduct);
     res.status(201).json(newProduct);
+});
+
+
+// PUT /products/:id - Update a product by ID
+router.put('/:id', (req, res) => {
+    const { name, descr, price } = req.body;
+
+// Buscar el producto por el id
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) {
+        return res.status(404).json({ error: 'Producto no encontrado.' });
+    }
+
+//estar seguro que al menos un campo esta proporcionado
+    if (!name && !descr && !price) {
+        return res.status(400).json({ error: 'No hay campos proporcionados para actualizar' });
+    }
+
+// Validaciones para los campos
+    if (name !== undefined) {
+        if (typeof name !== 'string' || name.trim().length === 0) {
+            return res.status(400).json({ error: 'Este campo es obligatorio y debe ser un texto.' });
+        }
+        product.name = name.trim();
+    }
+
+    if (descr !== undefined) {
+        if (typeof descr !== 'string' || descr.trim().length === 0) {
+            return res.status(400).json({ error: 'Debes ingresar una descripcion.' });
+        }
+        product.descr = descr.trim();
+    }
+
+    if (price !== undefined) {
+        if (isNaN(price) || price <= 0) {
+            return res.status(400).json({ error: 'Este campo es obligatorio y debe ser mayor a 0.' });
+        }
+        product.price = parseFloat(price);
+    }
+
+    res.status(200).json(product);
 });
 
 
